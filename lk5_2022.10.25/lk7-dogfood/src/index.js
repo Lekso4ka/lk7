d421f9b6f7062d227db36aff8372de21af39d087
Leksa <admin@petiteweb.ru>
2022-10-26 19:04:48 +0300

    Lesson 5

---
+++ lk5_2022.10.25/lk7-dogfood/src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+ReactDOM.render(
+	<div className="wrapper">
+		<div className="card">=)</div>	
+		<div className="card">O_o</div>	
+		<div className="card">^_^</div>	
+		<div className="card">=(</div>	
+	</div>,
+	document.querySelector("#root")
+)
